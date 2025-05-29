676c401bffa62dc742294d5dfb8f46b64c7a84da
이예지 <leeyeji@iyejiui-MacBookAir.local>
2025-05-29 17:39:09 +0900

    포트폴리오

---
+++ UMC과제/UMC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UMC과제
+//
+//  Created by 이예지 on 5/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UMC과제/UMC/UMC____App.swift
@@ -0,0 +1,12 @@
+//___FILEHEADER___
+
+import SwiftUI
+
+@main
+struct UMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
